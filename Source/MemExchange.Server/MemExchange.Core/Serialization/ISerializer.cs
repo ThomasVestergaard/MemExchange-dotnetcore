cf8093cf38ffe20f49a81268dd235b38fed9e99d
Thomas Vestergaard <mail@thomasvestergaard.com>
2018-11-24 23:18:23 +0100

    Porting core project.

---
+++ Source/MemExchange.Server/MemExchange.Core/Serialization/ISerializer.cs
@@ -0,0 +1,9 @@
+﻿namespace MemExchange.Core.Serialization
+{
+    public interface ISerializer
+    {
+        byte[] Serialize<T>(T inputInstance);
+        T Deserialize<T>(byte[] serializedData);
+        T Deserialize<T>(byte[] serializedData, int length);
+    }
+}
---
+++ Source/MemExchange.Server/MemExchange.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemExchange.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
