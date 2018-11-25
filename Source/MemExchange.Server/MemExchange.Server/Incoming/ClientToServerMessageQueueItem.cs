c14285b673d6b346e282d2393e366e4182a1f712
Thomas Vestergaard <mail@thomasvestergaard.com>
2018-11-25 21:45:59 +0100

    Added manual startup. Needs to be moved to an IOC container.

--- Source/MemExchange.Server/MemExchange.Server/Incoming/ClientToServerMessageQueueItem.cs
+++ Source/MemExchange.Server/MemExchange.Server/Incoming/ClientToServerMessageQueueItem.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using MemExchange.Core.SharedDto.ClientToServer;
+using ProtoBuf;
 
 namespace MemExchange.Server.Incoming
 {
+    [ProtoContract]
     public class ClientToServerMessageQueueItem
     {
+        [ProtoMember(1)]
         public ClientToServerMessage Message { get; set; }
+        [ProtoMember(2)]
         public DateTimeOffset StartProcessTime { get; set; }
 
         public ClientToServerMessageQueueItem()
--- Source/MemExchange.Server/MemExchange.Server/Program.cs
+++ Source/MemExchange.Server/MemExchange.Server/Program.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using MemExchange.Core.Logging;
+using MemExchange.Core.Serialization;
+using MemExchange.Server.Common;
+using MemExchange.Server.Incoming;
+using MemExchange.Server.Incoming.Logging;
+using MemExchange.Server.Outgoing;
+using MemExchange.Server.Processor;
+using MemExchange.Server.Processor.Book;
+using Serilog.Core;
 
 namespace MemExchange.Server
 {
@@ -6,7 +15,38 @@ namespace MemExchange.Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting...");
+
+
+            ILogger logger = new SerilogLogger();
+            ISerializer serializer = new ProtobufSerializer();
+            IMessagePublisher messagePublisher = new MessagePublisher(logger, serializer);
+            IOutgoingQueue outgoingQueue = new OutgoingQueue(logger, messagePublisher);
+
+            messagePublisher.Start(9193);
+            outgoingQueue.Start();
+
+            IOrderRepository orderRepository = new OrderRepository();
+            IOrderDispatcher orderDispatcher = new OrderDispatcher(outgoingQueue, logger, new DateService(), orderRepository);
+            IIncomingMessageProcessor incomingMessageProcessor = new IncomingMessageProcessor(orderRepository, outgoingQueue, new DateService(), orderDispatcher , new ProtobufSerializer());
+            IPerformanceRecorder performanceRecorder = new PerformanceRecorderDirectConsoleOutput(new DateService());
+            IIncomingMessageQueue incomingMessageQueue = new IncomingMessageQueue(logger, incomingMessageProcessor, performanceRecorder);
+            IClientMessagePuller clientMessagePuller = new ClientMessagePuller(logger, new ProtobufSerializer(), incomingMessageQueue);
+
+            incomingMessageQueue.Start();
+            clientMessagePuller.Start(9192);
+
+            Console.WriteLine("Started. Hit any key to quit.");
+            Console.ReadKey();
+
+            Console.WriteLine("Stopping...");
+
+            clientMessagePuller.Stop();
+            incomingMessageQueue.Stop();
+            outgoingQueue.Stop();
+            messagePublisher.Stop();
+
+            Console.WriteLine("Stopped");
         }
     }
 }
